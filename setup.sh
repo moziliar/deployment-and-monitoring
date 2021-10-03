eb30a62299928ceb72ca2ffafe7646052e5d7549
Mo Zongran <zongran.mo@shopee.com>
2021-10-03 16:50:43 +0800

    add setup script

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [[ ! -f .env ]]; then
+  cp .env.template .env
+fi
+
+if [[ ! -f machine-list.yaml ]]; then
+  touch machine-list.yaml
+fi
+
+if [[ ! -f software-list.yaml ]]; then
+  touch software-list.yaml
+fi
