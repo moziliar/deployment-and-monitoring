5b759235c04d89a03a6e064b62da956f90180b65
moziliar <mozongran@gmail.com>
2022-01-21 07:57:38 +0800

    add directory cleanup

--- monitoring/scripts/install_node_exporter.sh
+++ monitoring/scripts/install_node_exporter.sh
@@ -14,10 +14,10 @@ fi
 
 wget https://github.com/prometheus/node_exporter/releases/download/v1.2.2/node_exporter-1.2.2.linux-amd64.tar.gz
 tar xvfz node_exporter-1.2.2.linux-amd64.tar.gz
-cd node_exporter-1.2.2.linux-amd64 || echo Unpack failed && exit
+cd node_exporter-1.2.2.linux-amd64
 cp node_exporter /usr/local/bin/node_exporter
 cd ..
-rm -rf node_exporter-1.2.2.linux-amd64.tar.gz
+rm -rf node_exporter-1.2.2.linux-amd64*
 systemctl daemon-reload
 systemctl start node_exporter
 
--- monitoring/scripts/install_prometheus.sh
+++ monitoring/scripts/install_prometheus.sh
@@ -7,6 +7,10 @@ else
   cp ./template/prometheus.service /usr/lib/systemd/system/prometheus.service
 fi
 
+if [[ ! -d /etc/prometheus ]]; then
+  mkdir /etc/prometheus
+fi
+
 if [[ -e /usr/local/bin/prometheus ]]; then
   echo prometheus version "${/usr/local/bin/prometheus --version}" exists
   exit
@@ -14,11 +18,11 @@ fi
 
 wget https://github.com/prometheus/prometheus/releases/download/v2.31.0-rc.0/prometheus-2.31.0-rc.0.linux-amd64.tar.gz
 tar xvfz prometheus-2.31.0-rc.0.linux-amd64.tar.gz
-cd prometheus-2.31.0-rc.0.linux-amd64 || echo Unpack failed && exit
+cd prometheus-2.31.0-rc.0.linux-amd64
 cp prometheus /usr/local/bin/prometheus
 cp prometheus.yml /etc/prometheus/prometheus.yml # Manual setup for node exporter scraping required
 cd ..
-rm -rf prometheus-2.31.0-rc.0.linux-amd64.tar.gz
+rm -rf prometheus-2.31.0-rc.0.linux-amd64*
 systemctl daemon-reload
 systemctl start prometheus
 
