2bcbb7bfb3a2a03465b83149f45bb76449bbb980
moziliar <mozongran@gmail.com>
2022-01-21 16:23:03 +0800

    update reverse proxy setup script

--- monitoring/install.sh
+++ monitoring/install.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# Prepare systemd service file directory
+if [[ ! -d /usr/lib/systemd/system ]]; then
+    mkdir /usr/lib/systemd/system
+fi
+
 chmod +x scripts/install_prometheus.sh
 ./scripts/install_prometheus.sh
 
@@ -7,4 +12,4 @@ chmod +x scripts/install_node_exporter.sh
 ./scripts/install_node_exporter.sh
 
 chmod +x scripts/install_reverse_tunnel.sh
-./scripts/install_reverse_tunnel.sh
+./scripts/install_reverse_tunnel.sh $1 $2 $3
--- monitoring/scripts/install_reverse_tunnel.sh
+++ monitoring/scripts/install_reverse_tunnel.sh
@@ -4,7 +4,11 @@
 if [[ -e /usr/lib/systemd/system/node_exporter_reverse_tunnel.service ]]; then
   echo reverse tunnel service file exists
 else
+  TARGET_PORT=$1
+  KEY_FILE=$2
+  REMOTE_USER_HOST=$3
   cp ./template/node_exporter_reverse_tunnel.service /usr/lib/systemd/system/node_exporter_reverse_tunnel.service
+  sed -e "s/\${TARGET_PORT}/$TARGET_PORT/g" -e "s/\${KEY_FILE}/$KEY_FILE/g" -e "s/\${REMOTE_USER_HOST}/$REMOTE_USER_HOST/g" /usr/lib/systemd/system/node_exporter_reverse_tunnel.service
 fi
 
 systemctl daemon-reload
