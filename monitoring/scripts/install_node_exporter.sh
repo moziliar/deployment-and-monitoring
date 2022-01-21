a5bffbb2d1e28482b85652a8cc62d44875527841
moziliar <mozongran@gmail.com>
2022-01-21 08:12:59 +0800

    update installer with service users

--- monitoring/scripts/install_node_exporter.sh
+++ monitoring/scripts/install_node_exporter.sh
@@ -12,6 +12,9 @@ if [[ -e /usr/local/bin/node_exporter ]]; then
   exit
 fi
 
+# Create node exporter user
+useradd --no-create-home --shell /bin/false node_exporter
+
 wget https://github.com/prometheus/node_exporter/releases/download/v1.2.2/node_exporter-1.2.2.linux-amd64.tar.gz
 tar xvfz node_exporter-1.2.2.linux-amd64.tar.gz
 cd node_exporter-1.2.2.linux-amd64
--- monitoring/scripts/install_prometheus.sh
+++ monitoring/scripts/install_prometheus.sh
@@ -16,10 +16,16 @@ if [[ -e /usr/local/bin/prometheus ]]; then
   exit
 fi
 
+# Create prom user and change owner of dir
+useradd --no-create-home --shell /bin/false prometheus
+chown prometheus:prometheus /etc/prometheus
+chown prometheus:prometheus /var/lib/prometheus
+
 wget https://github.com/prometheus/prometheus/releases/download/v2.31.0-rc.0/prometheus-2.31.0-rc.0.linux-amd64.tar.gz
 tar xvfz prometheus-2.31.0-rc.0.linux-amd64.tar.gz
 cd prometheus-2.31.0-rc.0.linux-amd64
 cp prometheus /usr/local/bin/prometheus
+chown prometheus:prometheus /usr/local/bin/prometheus
 cp prometheus.yml /etc/prometheus/prometheus.yml # Manual setup for node exporter scraping required
 cd ..
 rm -rf prometheus-2.31.0-rc.0.linux-amd64*
