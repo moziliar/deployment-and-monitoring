56709aad29ab513a009176a202808c4305939de9
moziliar <mozongran@gmail.com>
2022-01-24 16:38:54 +0800

    update reverse proxy setup script sed delimiter

--- monitoring/scripts/install_reverse_tunnel.sh
+++ monitoring/scripts/install_reverse_tunnel.sh
@@ -8,10 +8,10 @@ else
   KEY_FILE=$2
   REMOTE_TARGET=$3
   REMOTE_USER=$4
-  sed -e "s/TARGET_PORT/$TARGET_PORT/g"\
-    -e "s/KEY_FILE/$KEY_FILE/g"\
-    -e "s/REMOTE_TARGET/$REMOTE_TARGET"\
-    -e "s/REMOTE_USER/REMOTE_USER/g"\
+  sed -e "s|TARGET_PORT|$TARGET_PORT|g"\
+    -e "s|KEY_FILE|$KEY_FILE|g"\
+    -e "s|REMOTE_TARGET|$REMOTE_TARGET|g"\
+    -e "s|REMOTE_USER|REMOTE_USER|g"\
     ./template/node_exporter_reverse_tunnel.service\
     > /usr/lib/systemd/system/node_exporter_reverse_tunnel.service
 fi
