1cf61f845f591c9b318f494ebc97e699f9a2b752
moziliar <mozongran@gmail.com>
2022-01-24 18:59:14 +0800

    update: add key gen and copy in reverse tunnel script

--- monitoring/scripts/install_reverse_tunnel.sh
+++ monitoring/scripts/install_reverse_tunnel.sh
@@ -4,10 +4,18 @@
 if [[ -e /usr/lib/systemd/system/node_exporter_reverse_tunnel.service ]]; then
   echo reverse tunnel service file exists
 else
+  # Set up node_exporter key
   TARGET_PORT=$1
   KEY_FILE=$2
   REMOTE_TARGET=$3
   REMOTE_USER=$4
+
+  if [[ ! -e $KEY_FILE ]]; then
+    ssh-keygen -f $KEY_FILE -q -N ""
+  fi
+
+  ssh-copy-id -i $KEY_FILE $REMOTE_USER@$REMOTE_TARGET
+
   sed -e "s|TARGET_PORT|$TARGET_PORT|g"\
     -e "s|KEY_FILE|$KEY_FILE|g"\
     -e "s|REMOTE_TARGET|$REMOTE_TARGET|g"\
