02c4a0a99bc2fbf1e0031540131fedd541a21cfb
moziliar <mozongran@gmail.com>
2022-03-25 17:05:07 +0800

    update: change password from stdin to from vault file

--- deployment/utils/run.py
+++ deployment/utils/run.py
@@ -6,6 +6,6 @@ def run_ansible_at(path):
         'ansible-playbook',
         path,
         '-f', '20',
-        '--become', '-K'
+        '--become', '--vault-password-file=vault.txt'
     ], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
     return sync_user_proc.communicate()
