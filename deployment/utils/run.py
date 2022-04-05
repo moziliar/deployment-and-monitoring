063ba1d8d0ee4153415bc319993c00e42ff0b63d
moziliar <mozongran@gmail.com>
2022-04-05 15:11:59 +0800

    update

--- deployment/utils/run.py
+++ deployment/utils/run.py
@@ -2,10 +2,9 @@ import subprocess
 
 
 def run_ansible_at(path):
-    sync_user_proc = subprocess.Popen([
+    return subprocess.run([
         'ansible-playbook',
         path,
         '-f', '20',
         '--become', '--vault-password-file=vault.txt'
     ], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-    return sync_user_proc.communicate()
